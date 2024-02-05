e683d319c9b468e2d3e2d389ce886db1ac27f65f
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2024-02-05 10:33:26 +0100

    Update builder.py

--- liesel_bctm/experimental/builder.py
+++ liesel_bctm/experimental/builder.py
@@ -3,11 +3,16 @@ from __future__ import annotations
 from typing import Callable
 
 import jax
+import jax.numpy as jnp
+from liesel_internal import splines
 
 from .. import tp_penalty as tpp
 from ..builder import CTMBuilder, MITEDerivative
 from ..custom_types import Array
+from ..distreg.mi_splines import MIPSplineTE1
 from . import mi_splines as mi
+
+kn = splines.create_equidistant_knots
 
 # flake8: noqa
 
@@ -24,11 +29,25 @@ class ExperimentalCTMBuilder(CTMBuilder):
         order: int = 3,
         positive_tranformation: Callable[[Array], Array] = jax.nn.softplus,
         name: str | None = None,
+        knot_boundaries: tuple[
+            tuple[float, float] | None, tuple[float, float] | None
+        ] = (None, None),
     ) -> CTMBuilder:
         str_name = self._pt_name(name, "teprod_mi1_full_experimental")
         x1val, x2val = self._array(x1), self._array(x2)
 
-        mite_spline = tpp.MIPSplineTE1(
+        bounds_y, bounds_x = knot_boundaries
+        if bounds_y is not None:
+            knots_y = kn(jnp.array(bounds_y), order=order, n_params=nparam[0])
+        else:
+            knots_y = None
+
+        if bounds_x is not None:
+            knots_x = kn(jnp.array(bounds_x), order=order, n_params=nparam[1])
+        else:
+            knots_x = None
+
+        mite_spline = MIPSplineTE1(
             str_name,
             x=(x1val, x2val),
             nparam=nparam,
@@ -37,6 +56,7 @@ class ExperimentalCTMBuilder(CTMBuilder):
             order=order,
             weights=weights,
             positive_tranformation=positive_tranformation,
+            knots=(knots_y, knots_x),
             Z=None,
         )
         self.pt.append(mite_spline)
